b3afe7367ca2dc5cdd6ef534e07f45aa9e4a258e
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2023-11-06 10:35:54 -0400

    Update Script Automação_excel().vb

--- Script Automação_excel().vb
+++ Script Automação_excel().vb
@@ -1,151 +1,135 @@
-'ultimate script que converte qualquer formato de documento pra qualquer outro formato suportado, abre, edita, fecha e salva qualquer modificação de planilhas excel dentro de uma pasta inteira de documentos!
-
 Sub AutomaçãoFoda()
     
     Dim FolderPathOld As String
-    'esta variável identifica a pasta que estão os documentos a serem convertidos para algum outro formato;
     Dim Filename As String
-    'esta variável identifica os arquivos dentro de uma pasta que sofrerão a conversão para outra extensão;
     Dim FolderPathNew As String
-    'esta variável identifica a pasta onde estão os documentos já convertidos que passarão por algum tratamento de dados da Macro;
     Dim wb As Workbook
-    'esta variável é responsável por abrir, salvar e fechar os arquivos;
     Dim FileNameFormatation As String
-    'esta variável é responsável por auxiliar a entrada e o fechamento do loop da macro 
     
+    FolderPathOld = "C:\Users\Richard.silva\Downloads\notacsv\"
+    FolderPathNew = "C:\Users\Richard.silva\Downloads\notaxlsx\"
     
-    'Defina o diretório da pasta onde estão os arquivos .csv
-    FolderPathOld = CUsersRichard.silvaDownloadsnotacsv
+    ' Identificando o arquivo
+    Filename = Dir(FolderPathOld & "*.csv")
     
-    'Especifique o diretório de destino para os arquivos XLSX
-    FolderPathNew = CUsersRichard.silvaDownloadsnotaxlsx
-    
-    'Inicialize a variável Filename
-    Filename = Dir(FolderPathOld & .csv)
-    
-    'Desative atualizações de tela para melhorar o desempenho
     Application.ScreenUpdating = False
     
-    'Percorra todos os arquivos .csv na pasta
-    Do While Filename  
-        'Use Workbooks.OpenText para importar o CSV diretamente como uma nova pasta de trabalho
-        Workbooks.OpenText Filename=FolderPathOld & Filename, DataType=xlDelimited, TextQualifier=xlDoubleQuote, ConsecutiveDelimiter=False, Tab=False, Semicolon=True, Comma=False, Space=False, Other=False, Local=True
+    Do While Filename <> ""
+        ' Usando Workbooks.OpenText para importar o CSV diretamente como uma nova pasta de trabalho
+        Workbooks.OpenText Filename:=FolderPathOld & Filename, DataType:=xlDelimited, TextQualifier:=xlDoubleQuote, ConsecutiveDelimiter:=False, Tab:=False, Semicolon:=True, Comma:=False, Space:=False, Other:=False, Local:=True
         
-        'Atualize o nome da pasta de trabalho para remover a extensão .csv
-        ActiveWorkbook.SaveAs Filename=FolderPathNew & Left(ActiveWorkbook.Name, Len(ActiveWorkbook.Name) - 4) & .xlsx, FileFormat=51 ' 51 é (pinga) o formato para .xlsx
-        ActiveWorkbook.Close SaveChanges=False
+        ' Atualizando o nome da pasta de trabalho para remover a extensão .csv
+        ActiveWorkbook.SaveAs Filename:=FolderPathNew & Left(ActiveWorkbook.Name, Len(ActiveWorkbook.Name) - 4) & ".xlsx", FileFormat:=51 ' 51 é o formato para .xlsx
+        ActiveWorkbook.Close SaveChanges:=False
         
-        'Obtenha o próximo arquivo .csv na pasta
+        ' Obter o próximo arquivo .csv na pasta
         Filename = Dir
     Loop
-    'Ative as atualizações de tela novamente
     
-    FileNameFormatation = Dir(FolderPathNew & .xlsx)
+    FileNameFormatation = Dir(FolderPathNew & "*.xlsx")
     
-    
-     Do While FileNameFormatation  
-        'Abra cada arquivo .xlsx
+    Do While FileNameFormatation <> ""
+        ' Abrir cada arquivo .xlsx
         Set wb = Workbooks.Open(FolderPathNew & FileNameFormatation)
         
-        'Ative a atualização da tela para a execução da macro
+        ' Ativar a atualização da tela para a execução da macro
         Application.ScreenUpdating = False
         
-        'Execute a macro na pasta de trabalho
-    Columns(AA).EntireColumn.AutoFit
-    Columns(BB).EntireColumn.AutoFit
-    Columns(CC).EntireColumn.AutoFit
-    Columns(DD).EntireColumn.AutoFit
-    Columns(EE).EntireColumn.AutoFit
-    Columns(FF).EntireColumn.AutoFit
-    Columns(GG).EntireColumn.AutoFit
-    Range(A1G1).Select
-    Range(G1).Activate
-    Selection.Font.Bold = True
-    Columns(AG).Select
-    Range(G18).Activate
-    With Selection
-        .HorizontalAlignment = xlGeneral
-        .VerticalAlignment = xlCenter
-        .WrapText = False
-        .Orientation = 0
-        .AddIndent = False
-        .IndentLevel = 0
-        .ShrinkToFit = False
-        .ReadingOrder = xlContext
-        .MergeCells = False
-    End With
-    With Selection
-        .HorizontalAlignment = xlCenter
-        .VerticalAlignment = xlCenter
-        .WrapText = False
-        .Orientation = 0
-        .AddIndent = False
-        .IndentLevel = 0
-        .ShrinkToFit = False
-        .ReadingOrder = xlContext
-        .MergeCells = False
-    End With
-    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
-    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
-    With Selection.Borders(xlEdgeLeft)
-        .LineStyle = xlContinuous
-        .ColorIndex = 0
-        .TintAndShade = 0
-        .Weight = xlThin
-    End With
-    With Selection.Borders(xlEdgeTop)
-        .LineStyle = xlContinuous
-        .ColorIndex = 0
-        .TintAndShade = 0
-        .Weight = xlThin
-    End With
-    With Selection.Borders(xlEdgeBottom)
-        .LineStyle = xlContinuous
-        .ColorIndex = 0
-        .TintAndShade = 0
-        .Weight = xlThin
-    End With
-    With Selection.Borders(xlEdgeRight)
-        .LineStyle = xlContinuous
-        .ColorIndex = 0
-        .TintAndShade = 0
-        .Weight = xlThin
-    End With
-    With Selection.Borders(xlInsideVertical)
-        .LineStyle = xlContinuous
-        .ColorIndex = 0
-        .TintAndShade = 0
-        .Weight = xlThin
-    End With
-    With Selection.Borders(xlInsideHorizontal)
-        .LineStyle = xlContinuous
-        .ColorIndex = 0
-        .TintAndShade = 0
-        .Weight = xlThin
-    End With
-    Columns(GG).Select
-    Selection.Style = Currency
-    Selection.ColumnWidth = 15.5
-    Columns(CC).Select
-    Selection.Style = Currency
-      Columns(DD).Select
-    Selection.Style = Currency
-    Selection.ColumnWidth = 80#
-    Columns(CC).Select
-    Selection.Style = Currency
-    
+        ' Executar a macro na pasta de trabalho
+        Columns("A:A").EntireColumn.AutoFit
+        Columns("B:B").EntireColumn.AutoFit
+        Columns("C:C").EntireColumn.AutoFit
+        Columns("D:D").EntireColumn.AutoFit
+        Columns("E:E").EntireColumn.AutoFit
+        Columns("F:F").EntireColumn.AutoFit
+        Columns("G:G").EntireColumn.AutoFit
+        Range("A1:G1").Select
+        Range("G1").Activate
+        Selection.Font.Bold = True
+        Columns("A:G").Select
+        Range("G18").Activate
+        With Selection
+            .HorizontalAlignment = xlGeneral
+            .VerticalAlignment = xlCenter
+            .WrapText = False
+            .Orientation = 0
+            .AddIndent = False
+            .IndentLevel = 0
+            .ShrinkToFit = False
+            .ReadingOrder = xlContext
+            .MergeCells = False
+        End With
+        With Selection
+            .HorizontalAlignment = xlCenter
+            .VerticalAlignment = xlCenter
+            .WrapText = False
+            .Orientation = 0
+            .AddIndent = False
+            .IndentLevel = 0
+            .ShrinkToFit = False
+            .ReadingOrder = xlContext
+            .MergeCells = False
+        End With
+        Selection.Borders(xlDiagonalDown).LineStyle = xlNone
+        Selection.Borders(xlDiagonalUp).LineStyle = xlNone
+        With Selection.Borders(xlEdgeLeft)
+            .LineStyle = xlContinuous
+            .ColorIndex = 0
+            .TintAndShade = 0
+            .Weight = xlThin
+        End With
+        With Selection.Borders(xlEdgeTop)
+            .LineStyle = xlContinuous
+            .ColorIndex = 0
+            .TintAndShade = 0
+            .Weight = xlThin
+        End With
+        With Selection.Borders(xlEdgeBottom)
+            .LineStyle = xlContinuous
+            .ColorIndex = 0
+            .TintAndShade = 0
+            .Weight = xlThin
+        End With
+        With Selection.Borders(xlEdgeRight)
+            .LineStyle = xlContinuous
+            .ColorIndex = 0
+            .TintAndShade = 0
+            .Weight = xlThin
+        End With
+        With Selection.Borders(xlInsideVertical)
+            .LineStyle = xlContinuous
+            .ColorIndex = 0
+            .TintAndShade = 0
+            .Weight = xlThin
+        End With
+        With Selection.Borders(xlInsideHorizontal)
+            .LineStyle = xlContinuous
+            .ColorIndex = 0
+            .TintAndShade = 0
+            .Weight = xlThin
+        End With
+        Columns("G:G").Select
+        Selection.Style = "Currency"
+        Selection.ColumnWidth = 15.5
+        Columns("C:C").Select
+        Selection.Style = "Currency"
+        Columns("D:D").Select
+        Selection.Style = "Currency"
+        Selection.ColumnWidth = 80
+        Columns("C:C").Select
+        Selection.Style = "Currency"
         
-        'Salve as alterações na pasta de trabalho
+        ' Salvar as alterações na pasta de trabalho
         wb.Save
         
-        'Feche a pasta de trabalho sem perguntar para salvar novamente
-        wb.Close SaveChanges=False
+        ' Fechar a pasta de trabalho sem perguntar para salvar novamente
+        wb.Close SaveChanges:=False
         
-        'Obtenha o próximo arquivo .xlsx na pasta
+        ' Obter o próximo arquivo .xlsx na pasta
         FileNameFormatation = Dir
     Loop
     
-'Ative as atualizações de tela novamente
+    ' Ativar as atualizações de tela novamente
     Application.ScreenUpdating = True
-        
 End Sub
+
